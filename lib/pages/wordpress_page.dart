72bf3d4f7ee906828c58597d9dce49d85368b29d
soyeduarde01 <soyeduardovicente@gmail.com>
2024-03-12 05:28:10 -0400

    colocar logo de cnd

--- lib/pages/wordpress_page.dart
+++ lib/pages/wordpress_page.dart
@@ -29,61 +29,73 @@ class _WordpressPageState extends State<WordpressPage> {
           ),
         ),
       ),
-      body: FutureBuilder(
-        future: api.fetchPosts(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            final List posts = snapshot.data;
-            final List displayedPosts = posts.take(3).toList();
-            return ListView.builder(
-              itemCount: displayedPosts.length,
-              itemBuilder: (BuildContext context, int index) {
-                var post = displayedPosts[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-                  child: Card(
-                    color: const Color.fromRGBO(247, 241, 241, 1), // Color de fondo azul
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(17.0), // Esquinas redondeadas
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Image.network(post['featured_image_url']),
-                        ListTile(
-                          title: Text(post['title']['rendered'], style: const TextStyle(fontWeight: FontWeight.bold, color: Color(0xFF8B0415),),),
-                          subtitle: Text(_stripHtmlTagsAndEntities(post['excerpt']['rendered'])), 
-                          
-                        ),
-                        TextButton(
-                          child: const Text(
-                            'Leer Más...',
-                            style: TextStyle(color: Color(0xFF8B0415)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(height: 30),
+          const Image(
+            image: AssetImage('assets/cdn_logo.png'),
+            width: 270.0,
+          ),
+          const SizedBox(height: 30),
+          Expanded(
+            child: FutureBuilder(
+              future: api.fetchPosts(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  final List posts = snapshot.data;
+                  final List displayedPosts = posts.take(3).toList();
+                  return ListView.builder(
+                    itemCount: displayedPosts.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var post = displayedPosts[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                        child: Card(
+                          color: const Color.fromRGBO(247, 241, 241, 1), // Color de fondo azul
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(17.0), // Esquinas redondeadas
                           ),
-                          onPressed: () async {
-                            final url = Uri.parse(post['link']);
-                            await launchUrl(
-                                  url,
-                                  mode: LaunchMode.externalApplication,
-                                );
-                          },
+                          child: Column(
+                            children: <Widget>[
+                              Image.network(post['featured_image_url']),
+                              ListTile(
+                                title: Text(post['title']['rendered'], style: const TextStyle(fontWeight: FontWeight.bold, color: Color(0xFF8B0415))),
+                                subtitle: Text(_stripHtmlTagsAndEntities(post['excerpt']['rendered'])), 
+                              ),
+                              TextButton(
+                                child: const Text(
+                                  'Leer Más...',
+                                  style: TextStyle(color: Color(0xFF8B0415)),
+                                ),
+                                onPressed: () async {
+                                  final url = Uri.parse(post['link']);
+                                  await launchUrl(
+                                    url,
+                                    mode: LaunchMode.externalApplication,
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
                         ),
-
-                      ],
-                    ),
-                  ),
-                );
+                      );
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
               },
-            );
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
 
 
 // Función para eliminar etiquetas HTML y entidades
